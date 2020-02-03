022df26b0e09213b967c745b10b3cdaeaacf0387
XaxaChat <Xaxa@i.ua>
2020-02-03 06:41:18 +0200

    Updates

---
+++ precache-manifest.4ba2c1db80cc0bdab01b1dbb6ed82831.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8b4320c7da40ae098d0a09c78d89fe4",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "3c08f70b6e3124160489",
+    "url": "/todo/static/css/main.e72bf92f.chunk.css"
+  },
+  {
+    "revision": "deda134965f4497ffd73",
+    "url": "/todo/static/js/2.3fbd05a9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todo/static/js/2.3fbd05a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c08f70b6e3124160489",
+    "url": "/todo/static/js/main.669b22e3.chunk.js"
+  },
+  {
+    "revision": "dd9adfdb09f83cb1a7aa",
+    "url": "/todo/static/js/runtime-main.f7a45fe9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.88a07779a180ccc2886e1b0b449fc44c.js"
+  "/todo/precache-manifest.4ba2c1db80cc0bdab01b1dbb6ed82831.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
