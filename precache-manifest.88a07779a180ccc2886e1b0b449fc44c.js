72d6f031e173f895d3c5d17c1d25bf570ad93dc1
XaxaChat <Xaxa@i.ua>
2020-02-03 06:37:21 +0200

    Updates

---
+++ precache-manifest.88a07779a180ccc2886e1b0b449fc44c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe7dbcd1d4312b08c288a8411deebc29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86b16045f5c389c3d7c7",
+    "url": "/static/css/main.e72bf92f.chunk.css"
+  },
+  {
+    "revision": "deda134965f4497ffd73",
+    "url": "/static/js/2.3fbd05a9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.3fbd05a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86b16045f5c389c3d7c7",
+    "url": "/static/js/main.4df227f7.chunk.js"
+  },
+  {
+    "revision": "452efe56c2036ff11208",
+    "url": "/static/js/runtime-main.83c66be5.js"
+  }
+]);
